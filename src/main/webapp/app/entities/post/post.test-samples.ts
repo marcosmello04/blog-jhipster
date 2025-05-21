7ba46180549c4d3ab82cf4c71b29320ef707a0a1
Marcos Mello <marcos.mello04@gmail.com>
2025-05-21 15:21:32 -0300

    final

---
+++ src/main/webapp/app/entities/post/post.test-samples.ts
@@ -0,0 +1,36 @@
+import dayjs from 'dayjs/esm';
+
+import { IPost, NewPost } from './post.model';
+
+export const sampleWithRequiredData: IPost = {
+  id: 8730,
+  title: 'lawful above unless',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2025-05-19T21:52'),
+};
+
+export const sampleWithPartialData: IPost = {
+  id: 28272,
+  title: 'unless or supportive',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2025-05-20T11:50'),
+};
+
+export const sampleWithFullData: IPost = {
+  id: 9917,
+  title: 'clearly darn icy',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2025-05-20T03:49'),
+};
+
+export const sampleWithNewData: NewPost = {
+  title: 'willfully settler',
+  content: '../fake-data/blob/hipster.txt',
+  date: dayjs('2025-05-19T21:33'),
+  id: null,
+};
+
+Object.freeze(sampleWithNewData);
+Object.freeze(sampleWithRequiredData);
+Object.freeze(sampleWithPartialData);
+Object.freeze(sampleWithFullData);
